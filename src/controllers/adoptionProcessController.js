e5190edbf370b35d2e2f0649150bfd4ce9050bfc
Leandro <leandromfrota@gmail.com>
2024-11-05 21:08:16 -0300

    created update,delete, getbyid adoption process controllers and routers

--- src/controllers/adoptionProcessController.js
+++ src/controllers/adoptionProcessController.js
@@ -10,4 +10,79 @@ async getProcessAdopt(req,res){
      }
      
 }
+async registerAdoptionProcess(req,res){
+     const {pet_id ,adopter_id,data_adocao} = req.body
+     try{
+          await prismaClient.adoptionProcess.create({
+               data: {pet_id,adopter_id,data_adocao}
+          })
+          return res.status(200).send("Adoption Procces register succesfully")
+     }catch(error){
+          return res.status(500).json({error:error.message})
+     }
+}
+     async getProcessAdoptById(req,res){
+          const {id} = req.params
+
+          const getProcessAdopt = await prismaClient.adoptionProcess.findUnique({
+               where: {id}
+          })
+
+          if(!getProcessAdopt){
+               return res.status(404).send("Procces not found")
+          }
+
+          try{
+               const adoptionProcess = await prismaClient.adoptionProcess.findUnique({
+                    where:{id}
+               })
+               return res.status(200).json(adoptionProcess)
+          }catch(error){
+               return res.status(500).send({error:error.message})
+          }
+          
+     }
+     async updateAdoptionProcess(req,res){
+          const {id} = req.params
+          const{pet_id ,adopter_id,data_adocao} = req.body
+
+          const getProcessAdopt = await prismaClient.adoptionProcess.findUnique({
+               where: {id}
+          })
+
+          if(!getProcessAdopt){
+               return res.status(404).send("Adoption process not found")
+          }
+
+          try{
+               await prismaClient.adoptionProcess.update({
+                    data:{pet_id ,adopter_id,data_adocao},
+                    where:{id}   
+               })
+               return res.status(200).send("Adoption process updated")
+          }catch(error){
+               return res.status(500).json({error: error.message})
+          }
+     }
+     async deleteAdoptionProcess(req,res){
+          const {id} = req.params
+          
+          const getProcessAdopt = await prismaClient.adoptionProcess.findUnique({
+               where:{id}
+          })
+
+          if(!getProcessAdopt){
+               return res.status(404).send("Adoption process not found")
+
+          }
+
+          try{
+               await prismaClient.adoptionProcess.delete({
+                    where:{id}
+               })
+               return res.status(200).send("Adoption deleted")
+          }catch(error){
+               return res.status(500).json({error: error.message})
+          }
+     }
 }
--- src/routers/adoptionProcessRouter.js
+++ src/routers/adoptionProcessRouter.js
@@ -5,6 +5,10 @@ const adoptionProcessRouter = Router()
 const adoptionProcessController = new AdoptionProcessController()
 
 adoptionProcessRouter.get('/',adoptionProcessController.getProcessAdopt)
+adoptionProcessRouter.post('/',adoptionProcessController.registerAdoptionProcess)
+adoptionProcessRouter.get('/:id',adoptionProcessController.getProcessAdoptById)
+adoptionProcessRouter.put('/:id',adoptionProcessController.updateAdoptionProcess)
+adoptionProcessRouter.delete('/:id',adoptionProcessController.deleteAdoptionProcess)
 
 
 export{adoptionProcessRouter}
